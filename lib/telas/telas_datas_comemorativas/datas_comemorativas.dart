bc2d937314775d36ccf73c53cef3b072e12cd15e
lucasgia5 <lucasrgiaquinto@gmail.com>
2024-11-06 12:10:19 -0300

    design de telas 2.0

--- lib/telas/telas_datas_comemorativas/datas_comemorativas.dart
+++ lib/telas/telas_datas_comemorativas/datas_comemorativas.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutterosemeador/telas/jogos_tela.dart';  // Importa o arquivo jogos_tela.dart
+import 'package:flutterosemeador/telas/jogos_tela.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
 
 void main() {
   runApp(const PuzzleGameApp());
@@ -19,7 +18,6 @@ class PuzzleGameApp extends StatelessWidget {
     );
   }
 }
-
 
 class DifficultySelectionScreenDatas extends StatelessWidget {
   const DifficultySelectionScreenDatas({Key? key}) : super(key: key);
@@ -121,7 +119,8 @@ class DifficultySelectionScreenDatas extends StatelessWidget {
 
 class PuzzleGameScreen extends StatefulWidget {
   final String difficulty;
-  const PuzzleGameScreen({Key? key, required this.difficulty}) : super(key: key);
+  const PuzzleGameScreen({Key? key, required this.difficulty})
+      : super(key: key);
 
   @override
   State<PuzzleGameScreen> createState() => _PuzzleGameScreenState();
@@ -129,15 +128,51 @@ class PuzzleGameScreen extends StatefulWidget {
 
 class _PuzzleGameScreenState extends State<PuzzleGameScreen> {
   final List<PuzzleLevel> _allLevels = [
-    PuzzleLevel(difficulty: 'Fácil', puzzle: 'N A T _ _', answer: 'NATAL', imagePath: 'assets/imagens/datas_comemorativas/papai_noel_correto.png'),
-    PuzzleLevel(difficulty: 'Fácil', puzzle: 'A N _ N _ _ O', answer: 'ANO NOVO', imagePath: 'assets/imagens/datas_comemorativas/ano_novo.png'),
-    PuzzleLevel(difficulty: 'Fácil', puzzle: 'P A _ C _ A', answer: 'PASCOA', imagePath: 'assets/imagens/datas_comemorativas/pascoa.png'),
-    PuzzleLevel(difficulty: 'Médio', puzzle: 'D I _ D _ S C R I _ _ Ç _ S', answer: 'DIA DAS CRIANCAS', imagePath: 'assets/imagens/datas_comemorativas/dia_das_craincas.png'),
-    PuzzleLevel(difficulty: 'Médio', puzzle: 'H A L _ _ W _ _ _', answer: 'HALLOWEEN', imagePath: 'assets/imagens/datas_comemorativas/halloween.png'),
-    PuzzleLevel(difficulty: 'Médio', puzzle: 'T I R _ _ _ N T E _', answer: 'TIRADENTES', imagePath: 'assets/imagens/datas_comemorativas/tiradentes.webp'),
-    PuzzleLevel(difficulty: 'Difícil', puzzle: 'P R O C _ _ M A Ç Ã _  D _ R E P _ _ _ _ C A', answer: 'PROCLAMACAO DA REPUBLICA', imagePath: 'assets/imagens/datas_comemorativas/proclamacao_certa.png'),
-    PuzzleLevel(difficulty: 'Difícil', puzzle: 'C O N S _ _ _ _ C I A  N E G_ _', answer: 'CONSCIENCIA NEGRA', imagePath: 'assets/imagens/datas_comemorativas/consciencia_certa.png'),
-    PuzzleLevel(difficulty: 'Difícil', puzzle: 'I N D E P _ _ _ D E N _ I A D _ B R A _ _', answer: 'INDEPENDENCIA DO BRASIL', imagePath: 'assets/imagens/datas_comemorativas/independencia.png'),
+    PuzzleLevel(
+        difficulty: 'Fácil',
+        puzzle: 'N A T _ _',
+        answer: 'NATAL',
+        imagePath: 'assets/imagens/datas_comemorativas/papai_noel_correto.png'),
+    PuzzleLevel(
+        difficulty: 'Fácil',
+        puzzle: 'A N _  N _ _ O',
+        answer: 'ANO NOVO',
+        imagePath: 'assets/imagens/datas_comemorativas/ano_novo.png'),
+    PuzzleLevel(
+        difficulty: 'Fácil',
+        puzzle: 'P A _ C _ A',
+        answer: 'PASCOA',
+        imagePath: 'assets/imagens/datas_comemorativas/pascoa.png'),
+    PuzzleLevel(
+        difficulty: 'Médio',
+        puzzle: 'D I _  D _ S C R I _ _ Ç _ S',
+        answer: 'DIA DAS CRIANCAS',
+        imagePath: 'assets/imagens/datas_comemorativas/dia_das_craincas.png'),
+    PuzzleLevel(
+        difficulty: 'Médio',
+        puzzle: 'H A L _ _ W _ _ _',
+        answer: 'HALLOWEEN',
+        imagePath: 'assets/imagens/datas_comemorativas/halloween.png'),
+    PuzzleLevel(
+        difficulty: 'Médio',
+        puzzle: 'T I R _ _ _ N T E _',
+        answer: 'TIRADENTES',
+        imagePath: 'assets/imagens/datas_comemorativas/tiradentes.webp'),
+    PuzzleLevel(
+        difficulty: 'Difícil',
+        puzzle: 'P R O C _ _ M A Ç Ã _  D _  R E P _ _ _ _ C A',
+        answer: 'PROCLAMACAO DA REPUBLICA',
+        imagePath: 'assets/imagens/datas_comemorativas/proclamacao_certa.png'),
+    PuzzleLevel(
+        difficulty: 'Difícil',
+        puzzle: 'C O N S _ _ _ _ C I A  N E G_ _',
+        answer: 'CONSCIENCIA NEGRA',
+        imagePath: 'assets/imagens/datas_comemorativas/consciencia_certa.png'),
+    PuzzleLevel(
+        difficulty: 'Difícil',
+        puzzle: 'I N D E P _ _ _ D E N _ I A  D _  B R A _ _',
+        answer: 'INDEPENDENCIA DO BRASIL',
+        imagePath: 'assets/imagens/datas_comemorativas/independencia.png'),
   ];
 
   late List<PuzzleLevel> _levels;
@@ -145,26 +180,29 @@ class _PuzzleGameScreenState extends State<PuzzleGameScreen> {
   String _userInput = '';
   String _errorMessage = '';
   final TextEditingController _controller = TextEditingController();
-  final FocusNode _focusNode = FocusNode(); // FocusNode para o foco automático
+  final FocusNode _focusNode = FocusNode();
 
   @override
   void initState() {
     super.initState();
-    _levels = _allLevels.where((level) => level.difficulty == widget.difficulty).toList();
+    _levels = _allLevels
+        .where((level) => level.difficulty == widget.difficulty)
+        .toList();
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      _focusNode.requestFocus(); // Coloca foco no campo de texto ao iniciar
+      _focusNode.requestFocus();
     });
   }
 
   @override
   void dispose() {
     _controller.dispose();
-    _focusNode.dispose(); // Libera o FocusNode
+    _focusNode.dispose();
     super.dispose();
   }
 
   String _normalize(String input) {
-    return input.toLowerCase()
+    return input
+        .toLowerCase()
         .replaceAll(RegExp(r'[áàãâä]'), 'a')
         .replaceAll(RegExp(r'[éèêë]'), 'e')
         .replaceAll(RegExp(r'[íìîï]'), 'i')
@@ -178,18 +216,20 @@ class _PuzzleGameScreenState extends State<PuzzleGameScreen> {
     final currentLevel = _levels[_currentLevelIndex];
     if (_normalize(_userInput) == _normalize(currentLevel.answer)) {
       setState(() {
-        _userInput = ''; // Limpa o texto armazenado
-        _controller.clear(); // Limpa o campo de entrada
-        _errorMessage = ''; // Limpa a mensagem de erro
-        _currentLevelIndex++;
-        _focusNode.requestFocus(); // Recoloca o foco no campo de entrada
+        _userInput = '';
+        _controller.clear();
+        _errorMessage = '';
+
+        if (_currentLevelIndex < _levels.length - 1) {
+          _currentLevelIndex++;
+          _focusNode.requestFocus();
+        } else {
+          _showCompletionDialog();
+        }
       });
-      if (_currentLevelIndex >= _levels.length) {
-        _showCompletionDialog();
-      }
     } else {
       setState(() {
-        _errorMessage = 'Palavra incorreta, tente novamente.';
+        _errorMessage = 'Resposta incorreta, tente novamente.';
       });
     }
   }
@@ -197,72 +237,107 @@ class _PuzzleGameScreenState extends State<PuzzleGameScreen> {
   void _showCompletionDialog() {
     showDialog(
       context: context,
-      builder: (_) => AlertDialog(
-        title: const Text('Parabéns!'),
-        content: const Text('Você completou todos os níveis!'),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.of(context, rootNavigator: true).pop(); // Fecha o diálogo
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => JogosTela()), // Navega para JogosTela
-              );
-            },
-            child: const Text('Voltar ao Menu'),
-          ),
-        ],
-      ),
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+          title: const Text('Parabéns!'),
+          content: const Text('Você completou todos os níveis!'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => PuzzleGameApp(),
+                          ),
+                        );
+              },
+              child: const Text('Jogar novamente'),
+            ),
+          ],
+        );
+      },
     );
   }
 
   @override
   Widget build(BuildContext context) {
     final currentLevel = _levels[_currentLevelIndex];
-
     return Scaffold(
-      appBar: AppBar(title: Text('Modo: ${widget.difficulty}')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              currentLevel.imagePath, // Imagem do nível
-              height: 150,
-            ),
-            const SizedBox(height: 20),
-            Text(
-              currentLevel.puzzle,
-              style: const TextStyle(fontSize: 28, letterSpacing: 2),
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: _controller,
-              focusNode: _focusNode, // Associa o FocusNode ao campo de texto
-              onChanged: (value) {
-                setState(() {
-                  _userInput = value;
-                });
-              },
-              onSubmitted: (_) => _checkAnswer(),
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Digite o nome do feriado',
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('Nível ${_currentLevelIndex + 1} - ${widget.difficulty}'),
+        backgroundColor: Colors.deepOrange,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                currentLevel.puzzle,
+                style: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
               ),
-              style: const TextStyle(fontSize: 20),
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: _checkAnswer,
-              child: const Text('Verificar'),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              _errorMessage,
-              style: const TextStyle(color: Colors.red, fontSize: 16),
-            ),
-          ],
+              const SizedBox(height: 20),
+              Image.asset(
+                currentLevel.imagePath,
+                width: 200,
+                height: 200, // Aumenta o tamanho da imagem
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                controller: _controller,
+                focusNode: _focusNode,
+                onChanged: (value) {
+                  setState(() {
+                    _userInput = value;
+                  });
+                },
+                onSubmitted: (_) => _checkAnswer(), // Envia a resposta com Enter
+                decoration: InputDecoration(
+                  labelText: 'Digite a resposta',
+                  errorText: _errorMessage.isNotEmpty ? _errorMessage : null,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _checkAnswer,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.deepOrange,
+                  padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+                child: const Text(
+                  'Verificar',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                _errorMessage,
+                style: const TextStyle(
+                  color: Colors.red,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
         ),
       ),
     );
